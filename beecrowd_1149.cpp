8557851670d701be429d066bf142f8082c8dab83
Atik Hasan <atikh4335@gmail.com>
2024-02-04 14:41:01 +0600

    .

---
+++ beecrowd_1149.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, n, sum = 0;
+
+    cin >> a >> n;
+    while (n <= 0)
+    {
+        cin >> n;}
+        for (int i = 1; i <= n; i++)
+        {
+            sum += a;
+            a++;
+        }
+    
+    cout << sum << endl;
+    return 0;
+}
