5311a91feb6aa1ed275e780784e0c92f80ee8f2d
Atik Hasan <atikh4335@gmail.com>
2024-02-13 23:53:05 +0600

    added new code

--- emni2.cpp
+++ emni2.cpp
@@ -8,7 +8,7 @@ void MUKU()
 {
     int num;
     cin >> num;
-    string sSTR = "zzz";
+    string sSTR = "zzz"; //sSTR means smallest string
     for (char c1 = 'a'; c1 <= 'z'; c1++)
     {
         for (char c2 = 'a'; c2 <= 'z'; c2++)
@@ -16,10 +16,10 @@ void MUKU()
             for (char c3 = 'a'; c3 <= 'z'; c3++)
             {
                 int cSUM = c1 - 'a' + 1 + c2 - 'a' + 1 + c3 - 'a' + 1;
-                if (cSUM == num)
+                if (cSUM == num)  //cSUM means current sum
                 {
                     string cSTR = string(1, c1) + string(1, c2) + string(1, c3);
-                    if (cSTR < sSTR)
+                    if (cSTR < sSTR) //surrent string
                         sSTR = cSTR;
                 }
             }
