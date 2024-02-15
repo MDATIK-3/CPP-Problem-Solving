2ebf0a1a14d2ff25f1f3ec89160faee9425d9565
Atik Hasan <atikh4335@gmail.com>
2024-02-15 23:51:26 +0600

    IDPC first problem

---
+++ easypeasy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define ll long long int
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+        if (a % 2 == 0 && b % 2 == 0)
+        {
+            ll result = abs(a - b) / 2;
+            cout << result << endl;
+        }
+        else if ((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1))
+        {
+            ll result = (abs(a - b) + 1) / 2;
+            cout << result << endl;
+        }
+        else if (a % 2 == 1 && b % 2 == 1)
+        {
+            ll result = (abs(a - b) + 2) / 2;
+            cout << result << endl;
+        }
+    }
+}
