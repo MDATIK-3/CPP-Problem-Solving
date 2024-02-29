30471abbd6ecd61c0c0ba9e07e9ecdc491311d6a
MDATIK-3 <atikh4335@gmail.com>
2024-02-29 22:34:30 +0600

    added new code

---
+++ 1288.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+using namespace std;
+
+/*int Vowel(string str)
+{
+    int len = str.length();
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            count++;
+    }
+    return count;
+}*/
+
+bool primeGen(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void MUKU()
+{
+    ll N;
+    cin >> N;
+    ll n = N / 2;
+    if (N % 2 == 0)
+        cout << (n + 1) * n << endl;
+    else
+    {
+        cout << (n * (n + 1)) + (n + 1) << endl;
+    }
+}
+
+int main()
+{
+    fastio();
+
+    int t;
+    cin >> t;
+
+    while (t-- > 0)
+    {
+        MUKU();
+    }
+
+    return 0;
+}
