5919727e7c46dea555a6152a60cb3d0b9081255c
MDATIK-3 <atikh4335@gmail.com>
2024-03-22 16:21:12 +0600

    solved bitwise AND (a& (a-1)) problem

---
+++ 1475_B.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+using namespace std;
+
+/*int Vowel(string str)
+{
+    int len = str.length();
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            count++;
+    }
+    return count;
+}*/
+
+bool primeGen(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void MUKU()
+{
+    int n, x, y, z;
+    cin >> n;
+    y = n % 2020;
+    x = abs((n - y) / 2020 - y);
+    if (x * 2020 + y * 2021 == n)
+        yes;
+    else
+        no;
+}
+
+int main()
+{
+    fastio();
+
+    int t;
+    cin >> t;
+
+    while (t-- > 0)
+    {
+        MUKU();
+    }
+
+    return 0;
+}
