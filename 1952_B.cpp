c6248ef154fa89d05761e48354a9abd4c6909aac
MDATIK-3 <atikh4335@gmail.com>
2024-04-02 22:34:57 +0600

    print yes

---
+++ 1952_B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+using namespace std;
+
+/*int Vowel(string str)
+{
+    int len = str.length();
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            count++;
+    }
+    return count;
+}
+ll DIV(ll a, ll b)
+{
+    return (a + b - 1) / b;
+}*/
+
+void MUKU()
+{
+    yes;
+    // string str;
+    // cin >> str;
+    // int len = str.length();
+    // bool flag = false;
+    // for (int i = 1; i < len; i++)
+    // {
+    //     if (str[i - 1] == 'i' && str[i] == 't')
+    //     {
+    //         flag = true;
+    //         break;
+    //     }
+    // }
+    // (flag) ? yes : no;
+}
+
+int main()
+{
+    fastio();
+
+    int t;
+    cin >> t;
+    while (t-- > 0)
+    {
+        MUKU();
+    }
+
+    return 0;
+}
